64fff25681194f6b33c5f3fa9a4be3532e649b81
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2020-09-26 09:49:38 +0700

    lab02

---
+++ ASP .Net/Lab02/WingtipToys/WingtipToys/Site.Master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WingtipToys.Models;
+using WingtipToys.Logic;
+namespace WingtipToys
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
+        }
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                string cartStr = string.Format("Cart ({0})", usersShoppingCart.GetCount());
+                cartCount.InnerText = cartStr;
+            }
+        }
+    }
+}
