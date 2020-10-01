8475b58dfe5fc693ed6f065b3edf77d518c0a105
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2020-10-01 09:08:47 +0700

    lab03

---
+++ ASP .Net/lab03/GolfClub/DAL/ClubMemberDbManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GolfClub.Model;
+
+namespace GolfClub.DAL
+{
+    public class ClubMemberDbManager
+    {
+        public Member CreateMember(Member memberModal)
+        {
+            using (var dbEntities = new GolfClubContext())
+            {
+                var MemberObj = new Member()
+                {
+                    Name = memberModal.Name,
+                    Gender = memberModal.Gender,
+                    MemberFee = memberModal.MemberFee,
+                };
+                dbEntities.Members.Add(MemberObj);
+                dbEntities.SaveChanges();
+            }
+            return memberModal;
+        } 
+        }
+}
---
+++ ASP .Net/lab03/GolfClub/DAL/GolfClubContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GolfClub.Model;
+using System.Data.Entity;
+namespace GolfClub.DAL
+{
+    public class GolfClubContext: DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+    }
+}
